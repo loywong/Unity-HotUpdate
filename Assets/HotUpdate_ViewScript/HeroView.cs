9ed0a35dd92797c21555bc25c998f223c8983eb6
LoyWong <loywongcom@gmail.com>
2026-01-27 19:25:00 +0800

    Add case for script on prefabs

---
+++ Assets/HotUpdate_ViewScript/HeroView.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class HeroView : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+        Debug.Log ("Hero, ViewScript 111");
+    }
+
+    public void Init () {
+        Debug.Log ("Init");
+    }
+}
